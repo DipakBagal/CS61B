af0e17c36d44018937ce60cdf7b3285268642bab
Ram Vishwanathan <ramvishwanathan@Ramas-MacBook-Air-2.local>
2020-11-01 13:40:44 +0530

    completed forst part of lab

---
+++ lab1/CuSum.java
@@ -0,0 +1,22 @@
+public class CuSum {
+
+  public static void main (String[] args){
+
+    int x = 0;
+    int s = 0;
+
+    while (x < 10){
+      s = s + x;
+      x = x + 1;
+
+      System.out.println(s);
+
+
+    }
+
+
+  }
+
+
+
+}
